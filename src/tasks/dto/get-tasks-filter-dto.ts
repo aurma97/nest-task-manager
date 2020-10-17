69e9ed74cc390c3ff675138052cfaad11a12b2e7
aurma97 <aurelienmarcel77@gmail.com>
2020-10-17 04:29:25 +0200

    Heheh

--- src/tasks/dto/get-tasks-filter-dto.ts
+++ src/tasks/dto/get-tasks-filter-dto.ts
@@ -1,6 +1,11 @@
+import { IsNotEmpty } from "class-validator";
 import { TaskStatus } from "../tasks.model";
 
 export class GetTasksFilterDto {
+    @IsNotEmpty()
     status: TaskStatus;
+
+    @IsNotEmpty()
     search: string;
 }
+
